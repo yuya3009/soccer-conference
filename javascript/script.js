7d2ced8fc423396361f7f244c2fb176943cac5e5
unknown <yoko3057co@gmail.com>
2021-07-13 17:38:52 +0900

    javascript start

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,7 @@
 'use strict';
 
-const lang = document.querySelector('html').lang;
-
-if(lang === 'ja') {
-  document.querySelector('option[value="index.html"]').selected = true;
-} else if(lang === 'en') {
-  document.querySelector('option[value="news.html"]').selected = true;
-} else if(lang === 'zh') {
-  document.querySelector('option[value="conference.html]').selected = true;
-}
-
-document.getElementById('form').select.onchange = function() {
-  location.href = document.getElementById('form').select.value;
-}
-
+$(function() {
+  $.ajaxSetup({
+    headers : {"X-Auth-Token" : "c78a1e5c0425431484aca90309e9d7da"}
+  })
+})
