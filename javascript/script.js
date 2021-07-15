b98640ae89c722d604dc420b05c8fe47a41d1fa7
unknown <yoko3057co@gmail.com>
2021-07-15 14:12:25 +0900

    JSON

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,23 @@
 'use strict';
 
-$(function() {
-  $.ajaxSetup({
-    headers : {"X-Auth-Token" : "c78a1e5c0425431484aca90309e9d7da"}
+$.ajax({
+  headers: { 'X-Auth-Token': 'APIキー' },
+  url: 'http://api.football-data.org/v2/matches?status='LIVE',
+  dataType: 'json',
+  type: 'GET',
+}).done(function(response) {
+  console.log(response);
+});
+
+$.getJSON('https://api.football-data.org/v2/competitions/CL/matches', function(data_cl) {
+  //JSON取得後の処理
+  matches = data_cl.matches[0].table;
+  //順位表にデータを入れる
+  matches.forEach(function(match) {
+    $('#cl-match').append(
+      '<tr align="center">'
+      + '<td>' + match.team + '</td>'
+      + '<td>' + match.time + '</td>'
+    )
   })
 })
